792d8c89605df2023b539db507179e26243f69f0
Nick Playfair <nick@chaosemerald.co.uk>
2018-08-19 14:37:47 +0100

    handle errors

--- bitcoinprice/script2.js
+++ bitcoinprice/script2.js
@@ -2,10 +2,19 @@ var btn = document.querySelector("#getPrice");
 
 btn.addEventListener("click", function() {
   fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  .then(handleErrors)
   .then(response => {
     return response.json();
   })
   .then(jsonPrice => {
     document.querySelector("#price").innerHTML = '&pound;' + jsonPrice.bpi.GBP.rate;
-  });
+  })
+  .catch(error => console.log(error));
 });
+
+function handleErrors (request){
+  if(!request.ok) {
+    throw Error(request.status);
+  }
+  return request;
+}
